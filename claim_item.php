6194ba6577fb0f42ca220a78a7a7f6e9bc1d6bc0
robloxrobo <tahnimaislamneha.edu@gmail.com>
2026-04-28 01:11:06 +0600

    Update claim_item.php

--- claim_item.php
+++ claim_item.php
@@ -2,7 +2,7 @@
 session_start();
 include 'db_connect.php';
 
-$current_user_id = 1; // replace with $_SESSION['user_id'] later
+$current_user_id = $_SESSION['user_id'];
 
 $message = "";
 $error   = "";
